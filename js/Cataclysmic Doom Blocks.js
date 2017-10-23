da6def4b41711436474f0934ee25f9b4fbd9c4b5
zyster <MARSICZA@mackillop.act.edu.au>
2017-10-24 09:46:29 +1100

    Added Ajax, fixed typos in loading sprites

--- js/Cataclysmic Doom Blocks.js
+++ js/Cataclysmic Doom Blocks.js
@@ -3,7 +3,7 @@ window.onload = function() {
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {preload: preload, create: create, update: update});
 
 //Global Variables
-var Ajax;
+var ajax;
 var Block;
 var Bomb;
 var DBlock;
@@ -18,31 +18,33 @@ var lives = 1;
 var livesText;
 
 function preload() {
-  game.load.image('Ajax', 'assets/Ajax.png');
-  game.load.image('Block', 'assets/Block.png');
-  game.load.image('Bomb', 'asstes/Bomb.png');
+  game.load.image('ajax', 'assets/Ajax.png');
+  game.load.image('block', 'assets/Block.png');
+  game.load.image('bomb', 'assets/Bomb.png');
   game.load.image('DBlock', 'assets/DBlock.png');
-  game.load.image('Explosion', 'assets/Explosion.png');
-  game.load.image('Laser', 'assets/Laser.png');
-  game.load.image('MedKit', 'assets/MedKit.png');
-  game.load.image('Missile', 'assets/Missile.png');
-  game.load.image('Shield', 'assets/Shield.png');
+  game.load.image('explosion', 'assets/Explosion.png');
+  game.load.image('laser', 'assets/Laser.png');
+  game.load.image('medKit', 'assets/Med Kit.png');
+  game.load.image('missile', 'assets/Missile.png');
+  game.load.image('shield', 'assets/Shield.png');
  }
 
 function create() {
-//Text
-livesText = game.add.text(20,game.world.height-100,'Lives: ' + lives, {fill: 'red'});
-scoreText = game.add.text(20,game.world.height-125,'Score: ' + score, {fill: 'orange'});
-//Weapon Setup
-weapon = game.add.weapon(100, 'Laser');
-weapon.bulletKillType = Phaser.Weapon.KILL_WORLD_BONUS;
-weapon.bulletSpeed = 1500;
-weapon.fireRate = 1500;
-weapon.bulletRotateToVelocity = true;
-weapon.trackSprite(Ajax);
+  //Creating the player
+  ajax = game.add.sprite(64, 64, 'ajax');
+  //Text
+  livesText = game.add.text(650,game.world.height-50,'Lives: ' + lives, {fill: 'red'});
+  scoreText = game.add.text(650,game.world.height-75,'Score: ' + score, {fill: 'orange'});
+  //Weapon Setup
+  //weapon = game.add.weapon(100, 'Laser');
+  //weapon.bulletKillType = Phaser.Weapon.KILL_WORLD_BONUS;
+  //weapon.bulletSpeed = 1500;
+  //weapon.fireRate = 1500;
+  //weapon.bulletRotateToVelocity = true;
+  //weapon.trackSprite(Ajax);
 }
 
 function update() {
-  
+
 }
 };
