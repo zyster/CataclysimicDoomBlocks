728f480b0201ce5f0818a44560c51c10a875711b
zyster <MARSICZA@mackillop.act.edu.au>
2017-10-24 12:32:47 +1100

    Add files via upload

--- js/Cataclysmic Doom Blocks.js
+++ js/Cataclysmic Doom Blocks.js
@@ -16,15 +16,17 @@ var score = 0;
 var scoreText;
 var lives = 1;
 var livesText;
+var playerbullet;
 
 function preload() {
   game.load.image('background', 'assets/Background.png');
   game.load.image('ajax', 'assets/Ajax.png');
+  game.load.image('ajaxN', 'assets/AjaxN.png')
   game.load.image('block', 'assets/Block.png');
   game.load.image('bomb', 'assets/Bomb.png');
   game.load.image('DBlock', 'assets/DBlock.png');
   game.load.image('explosion', 'assets/Explosion.png');
-  game.load.image('laser', 'assets/Laser.png');
+  game.load.image('playerbullet', 'assets/Laser.png');
   game.load.image('medKit', 'assets/Med Kit.png');
   game.load.image('missile', 'assets/Missile.png');
   game.load.image('shield', 'assets/Shield.png');
@@ -33,20 +35,20 @@ function preload() {
 function create() {
   //Adding Background
   background = game.add.sprite(0,0,'background');
+
   //Creating the player
   ajax = game.add.sprite(64, 64, 'ajax');
   game.physics.arcade.enable(ajax);
   ajax.body.collideWorldBounds = true;
+
   //Text
   livesText = game.add.text(650,game.world.height-50,'Lives: ' + lives, {fill: 'red'});
   scoreText = game.add.text(650,game.world.height-75,'Score: ' + score, {fill: 'orange'});
+
   //Weapon Setup
-  //weapon = game.add.weapon(100, 'Laser');
-  //weapon.bulletKillType = Phaser.Weapon.KILL_WORLD_BONUS;
-  //weapon.bulletSpeed = 1500;
-  //weapon.fireRate = 1500;
-  //weapon.bulletRotateToVelocity = true;
-  //weapon.trackSprite(Ajax);
+  playerbullet = game.add.group();
+  playerbullet.enableBody = true;
+  playerbullet.scale.setTo(1,1);
 
   //Creating controls
   moveKeys = game.input.keyboard.addKeys(
@@ -58,9 +60,9 @@ function create() {
 
 function update() {
   if (moveKeys.up.isDown || moveKeys.up2.isDown) {
-    ajax.y -= 4;
+    ajax.y -= 7;
 } else {
-  ajax.y += 3;
+  ajax.y += 5;
 }
 
 
